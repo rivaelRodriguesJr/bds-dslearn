63d5115db127260a8f9208f79481a3a6de294007
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2021-02-04 16:25:32 -0300

    Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -1,32 +1,33 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_course")
-public class Course implements Serializable {
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	private String imgUri;
-	private String imgGrayUri;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
 	
-	@OneToMany(mappedBy = "course")
-	private List<Offer> offers = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "course_id")
+	private Course course;
 	
-	public Course() {
+	public Offer() {
 	}
 
 	public Long getId() {
@@ -37,32 +38,36 @@ public class Course implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getEdition() {
+		return edition;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setEdition(String edition) {
+		this.edition = edition;
 	}
 
-	public String getImgUri() {
-		return imgUri;
+	public Instant getStartMoment() {
+		return startMoment;
 	}
 
-	public void setImgUri(String imgUri) {
-		this.imgUri = imgUri;
+	public void setStartMoment(Instant startMoment) {
+		this.startMoment = startMoment;
 	}
 
-	public String getImgGrayUri() {
-		return imgGrayUri;
+	public Instant getEndMoment() {
+		return endMoment;
 	}
 
-	public void setImgGrayUri(String imgGrayUri) {
-		this.imgGrayUri = imgGrayUri;
+	public void setEndMoment(Instant endMoment) {
+		this.endMoment = endMoment;
 	}
 
-	public List<Offer> getOffers() {
-		return offers;
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
 	}
 
 	@Override
@@ -81,7 +86,7 @@ public class Course implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Course other = (Course) obj;
+		Offer other = (Offer) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -89,5 +94,5 @@ public class Course implements Serializable {
 			return false;
 		return true;
 	}
-		
+	
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Programming.me.png', 'https://static.thenounproject.com/png/1574560-200.png');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
